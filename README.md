23dfe9f4ea670c14516ac77a0e8bbedb867f0174
Akilan Narayanaswamy <akilan98@gmail.com>
2015-08-29 18:47:33 +0000

    Header and navbar made responsive

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-This is the temporary home of the new website for the Greater Atlanta Tamil Sangam.
-
-A theme is being constructed for the new design mandated by the Website Sub-Committee, found here: gatamilsangam.org/newdesign.
-
+This is the new website for the Greater Atlanta Tamil Sangam. It is wordpress compatible with the new theme (found here: gatamilsangam.org/newdesign) implemented.
 --
-Akilan
+akilan98
--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -34,37 +34,38 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="" role="banner">
-			<div class="container" style="position:relative;left:60px">
+			<div id="header-container" class="container-fluid">
 				<div class="row">
-				<div class="GATSLogo col-md-4"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="/images/logo.jpg" border="0"></a></div>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<div class="GATSNameSection col-md-4">
-						<div class="GATSTamil">அட்லாண்டா மாநகரத் தமிழ்ச் சங்கம்</div>
-						<div class="GATSEnglish"><span><?php bloginfo( 'name' ); ?></span></div>
-						<div><span><?php bloginfo( 'description'); ?></span><br><span style="padding-right:20px">Cultural</span> <span style="padding-right:20px">Charitable</span> <span>Tamil Education</span></div>
+					<div id="name-logo" class=col-md-8>
+						<div class="GATSLogo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="/images/logo.jpg" border="0"></a></div>
+						<div class="GATSNameSection">
+							<div class="GATSTamil">அட்லாண்டா மாநகரத் தமிழ்ச் சங்கம்</div>
+							<div class="GATSEnglish"><span><?php bloginfo( 'name' ); ?></span></div>
+							<div><span><?php bloginfo( 'description'); ?></span><br><span style="padding-right:20px">Cultural</span> <span style="padding-right:20px">Charitable</span> <span>Tamil Education</span></div>
+						</div>
 					</div>
-				</a>
-				<div class="banner-link-container col-md-4">
-					<ul class="top-menu-links">
-						<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
-						<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Members</a></li>
-						<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">About</a></li>
-						<li style="border:none"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Contact</li></a>
-					</ul>
-					<ul class="no-border">
-						<li><a href="" target="_blank"><img src="http://www.gatamilsangam.org/images/newslettersignup.jpg" class="img-responsive" style="height:70px"></a></li>
-						<li><a href="http://www.gatamilsangam.org/VasanthaMalarl/2014/vmoct2015.pdf" target="_blank"><img title="Vasantha Malar Issue" src="http://www.gatamilsangam.org/VasanthaMalar/vmissue.png" style="height:70px"></a></li>
-						<li><a href="https://www.facebook.com/groups/192364347457395/"target="_blank"><img title="GATS Facebook Page" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTaxU_D5YlSbes17k_bOVCCQxCYLzYh8Ze_Pd5VkumyJjr0BdgIz4BJnEA" style="height:70px"></a></li>
-					</ul>
+					<div class="col-xs-6 col-md-4 banner-link-container">
+							<ul class="top-menu-links">
+								<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a></li>
+								<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Members</a></li>
+								<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">About</a></li>
+								<li style="border:none"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Contact</li></a>
+							</ul>
+							<br>
+							<ul class="no-border">
+								<li><a href="" target="_blank"><img src="http://www.gatamilsangam.org/images/newslettersignup.jpg" class="img-responsive" style="height:70px"></a></li>
+								<li><a href="http://www.gatamilsangam.org/VasanthaMalarl/2014/vmoct2015.pdf" target="_blank"><img title="Vasantha Malar Issue" src="http://www.gatamilsangam.org/VasanthaMalar/vmissue.png" style="height:70px"></a></li>
+								<li><a href="https://www.facebook.com/groups/192364347457395/"target="_blank"><img title="GATS Facebook Page" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTaxU_D5YlSbes17k_bOVCCQxCYLzYh8Ze_Pd5VkumyJjr0BdgIz4BJnEA" style="height:70px"></a></li>
+							</ul>
+					</div>
 				</div>
-				</div>
+				<nav id="navbar" class="navbar">
+					<div id="site-navigation" class="navigation main-navigation" role="navigation">
+						<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
+						<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
+						<div><?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu',  'menu_id' => 'primary-menu' ) ); ?></div>
+					</div><!-- #site-navigation -->
+				</nav><!-- #navbar -->
 			</div>
 		</header><!-- #masthead -->
-		<nav id="navbar" class="navbar">
-			<div id="site-navigation" class="navigation main-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
-				<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
-				<div><?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu',  'menu_id' => 'primary-menu' ) ); ?></div>
-			</div><!-- #site-navigation -->
-		</nav><!-- #navbar -->
 		<div id="main" class="site-main">
